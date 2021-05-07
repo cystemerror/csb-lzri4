6dc1074a367875b3db2dee1a0a723b68eb4c2e23
Code Sandbox <hello@codesandbox.io>
2021-05-07 19:53:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7d82156b619836b5ae40c7f19ef54985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff775fa35e8f9729a4ef",
+    "url": "https://cystemerror.github.io/csb-lzri4/static/css/main.6fa9d043.chunk.css"
+  },
+  {
+    "revision": "ff775fa35e8f9729a4ef",
+    "url": "https://cystemerror.github.io/csb-lzri4/static/js/main.ff775fa3.chunk.js"
+  },
+  {
+    "revision": "10d287770d84daf1573c",
+    "url": "https://cystemerror.github.io/csb-lzri4/static/js/1.10d28777.chunk.js"
+  },
+  {
+    "revision": "e93ce8515170e55ab9b7",
+    "url": "https://cystemerror.github.io/csb-lzri4/static/js/runtime~main.e93ce851.js"
+  },
+  {
+    "revision": "654bd0eedfdd557de72454cc0a0e42b6",
+    "url": "https://cystemerror.github.io/csb-lzri4/static/media/logo.654bd0ee.svg"
+  },
+  {
+    "revision": "f8e3ae43803f0e054ff07feb98f36514",
+    "url": "https://cystemerror.github.io/csb-lzri4/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://cystemerror.github.io/csb-lzri4/precache-manifest.7d82156b619836b5ae40c7f19ef54985.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://cystemerror.github.io/csb-lzri4/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
